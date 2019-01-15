074bcb1f88527884d108eaef30a820797aacbb67
Joko Irianto <irianto94@outlook.com>
2019-01-15 07:53:47 +0700

    change router to httprouter

--- interfaces/handler.go
+++ interfaces/handler.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/jojoarianto/go-ddd-api/application"
-	router "github.com/takashabe/go-router"
+	"github.com/julienschmidt/httprouter"
 )
 
 // Run start server
@@ -16,16 +17,26 @@ func Run(port int) error {
 }
 
 // Routes returns the initialized router
-func Routes() *router.Router {
-	r := router.NewRouter()
-	r.Get("/news/:id", getNews)
+func Routes() *httprouter.Router {
+	r := httprouter.New()
+	r.GET("/api/v1/news/:news_id", getNews)
 
 	return r
 }
 
+// =============================
+//    NEWS
+// =============================
+
 // Handler for get all news
-func getNews(w http.ResponseWriter, r *http.Request, id int) {
-	news, err := application.GetNews(id)
+func getNews(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	newsID, err := strconv.Atoi(ps.ByName("news_id"))
+	if err != nil {
+		Error(w, http.StatusNotFound, err, "invalid parameter")
+		return
+	}
+
+	news, err := application.GetNews(newsID)
 	if err != nil {
 		Error(w, http.StatusNotFound, err, "failed to get news")
 		return
@@ -33,3 +44,7 @@ func getNews(w http.ResponseWriter, r *http.Request, id int) {
 
 	JSON(w, http.StatusOK, news)
 }
+
+// =============================
+//    TOPIC
+// =============================
